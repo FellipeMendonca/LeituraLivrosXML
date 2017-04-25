ee63803a5d8134503a2940733c565959bddfdd39
Fellipe Mendonça <fellipekatana@gmai..com>
2017-04-25 11:38:28 -0300

    Fazendo metodo de pesquisa

--- LeituraLivrosXML/Manipuladores/LerArquivo.cs
+++ LeituraLivrosXML/Manipuladores/LerArquivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace LeituraLivrosXML.Manipuladores
 {
     public class LerArquivo
     {
+        private static string caminhoBiblia = AppDomain.CurrentDomain + "Biblia.txt";
+        private static string caminhoAlcorao = AppDomain.CurrentDomain + "Alcorao.txt";
+
+        public static string LerBiblia(string livro, string capitulo, string versiculo)
+        {
+            string livroUpper = livro.ToUpper(); // No txt o padrao é uppercase no nome dos livros
+            string capituloBusca = livroUpper + " [" + capitulo + "]"; // padrao GÊNESIS[1]
+            // Auxiliares de pesquisa
+            bool acheiLivro = false;
+            bool acheiCapitulo = false;
+            bool acheiversiculo = false;
+            // Retorno
+            string versiculoEncontrado = "";
+
+
+
+            return versiculoEncontrado;
+        }
+
     }
 }
--- LeituraLivrosXML/Principal.Designer.cs
+++ LeituraLivrosXML/Principal.Designer.cs
@@ -30,23 +30,23 @@
         {
             this.tbcPrincipal = new System.Windows.Forms.TabControl();
             this.tbpBiblia = new System.Windows.Forms.TabPage();
-            this.tbpAlcorao = new System.Windows.Forms.TabPage();
-            this.tbpNotas = new System.Windows.Forms.TabPage();
-            this.dgvNotas = new System.Windows.Forms.DataGridView();
-            this.lblSurata = new System.Windows.Forms.Label();
-            this.cbxSurata = new System.Windows.Forms.ComboBox();
-            this.lblVersoAlcorao = new System.Windows.Forms.Label();
-            this.txbVersiculoAlcorao = new System.Windows.Forms.TextBox();
-            this.btnProcuraAlcorao = new System.Windows.Forms.Button();
-            this.lblLivro = new System.Windows.Forms.Label();
-            this.txbCapituloBiblia = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnProcuraBiblia = new System.Windows.Forms.Button();
+            this.cbxLivrosBiblia = new System.Windows.Forms.ComboBox();
             this.txbVersiculoBiblia = new System.Windows.Forms.TextBox();
             this.lblVersiculo = new System.Windows.Forms.Label();
-            this.cbxLivrosBiblia = new System.Windows.Forms.ComboBox();
-            this.btnProcuraBiblia = new System.Windows.Forms.Button();
-            this.btnExcluirNota = new System.Windows.Forms.Button();
+            this.txbCapituloBiblia = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblLivro = new System.Windows.Forms.Label();
+            this.tbpAlcorao = new System.Windows.Forms.TabPage();
+            this.btnProcuraAlcorao = new System.Windows.Forms.Button();
+            this.txbVersiculoAlcorao = new System.Windows.Forms.TextBox();
+            this.lblVersoAlcorao = new System.Windows.Forms.Label();
+            this.cbxSurata = new System.Windows.Forms.ComboBox();
+            this.lblSurata = new System.Windows.Forms.Label();
+            this.tbpNotas = new System.Windows.Forms.TabPage();
             this.btnEditarNota = new System.Windows.Forms.Button();
+            this.btnExcluirNota = new System.Windows.Forms.Button();
+            this.dgvNotas = new System.Windows.Forms.DataGridView();
             this.tbcPrincipal.SuspendLayout();
             this.tbpBiblia.SuspendLayout();
             this.tbpAlcorao.SuspendLayout();
@@ -82,113 +82,25 @@
             this.tbpBiblia.Text = "Biblia";
             this.tbpBiblia.UseVisualStyleBackColor = true;
             // 
-            // tbpAlcorao
+            // btnProcuraBiblia
             // 
-            this.tbpAlcorao.Controls.Add(this.btnProcuraAlcorao);
-            this.tbpAlcorao.Controls.Add(this.txbVersiculoAlcorao);
-            this.tbpAlcorao.Controls.Add(this.lblVersoAlcorao);
-            this.tbpAlcorao.Controls.Add(this.cbxSurata);
-            this.tbpAlcorao.Controls.Add(this.lblSurata);
-            this.tbpAlcorao.Location = new System.Drawing.Point(4, 22);
-            this.tbpAlcorao.Name = "tbpAlcorao";
-            this.tbpAlcorao.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpAlcorao.Size = new System.Drawing.Size(252, 211);
-            this.tbpAlcorao.TabIndex = 1;
-            this.tbpAlcorao.Text = "Alcorão";
-            this.tbpAlcorao.UseVisualStyleBackColor = true;
+            this.btnProcuraBiblia.Location = new System.Drawing.Point(90, 165);
+            this.btnProcuraBiblia.Name = "btnProcuraBiblia";
+            this.btnProcuraBiblia.Size = new System.Drawing.Size(75, 23);
+            this.btnProcuraBiblia.TabIndex = 7;
+            this.btnProcuraBiblia.Text = "Procurar";
+            this.btnProcuraBiblia.UseVisualStyleBackColor = true;
+            this.btnProcuraBiblia.Click += new System.EventHandler(this.btnProcuraBiblia_Click);
             // 
-            // tbpNotas
+            // cbxLivrosBiblia
             // 
-            this.tbpNotas.Controls.Add(this.btnEditarNota);
-            this.tbpNotas.Controls.Add(this.btnExcluirNota);
-            this.tbpNotas.Controls.Add(this.dgvNotas);
-            this.tbpNotas.Location = new System.Drawing.Point(4, 22);
-            this.tbpNotas.Name = "tbpNotas";
-            this.tbpNotas.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpNotas.Size = new System.Drawing.Size(252, 211);
-            this.tbpNotas.TabIndex = 2;
-            this.tbpNotas.Text = "Notas";
-            this.tbpNotas.UseVisualStyleBackColor = true;
-            // 
-            // dgvNotas
-            // 
-            this.dgvNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvNotas.Location = new System.Drawing.Point(6, 6);
-            this.dgvNotas.Name = "dgvNotas";
-            this.dgvNotas.Size = new System.Drawing.Size(240, 169);
-            this.dgvNotas.TabIndex = 0;
-            // 
-            // lblSurata
-            // 
-            this.lblSurata.AutoSize = true;
-            this.lblSurata.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblSurata.Location = new System.Drawing.Point(6, 25);
-            this.lblSurata.Name = "lblSurata";
-            this.lblSurata.Size = new System.Drawing.Size(136, 17);
-            this.lblSurata.TabIndex = 0;
-            this.lblSurata.Text = "Selecione a Surata: ";
-            // 
-            // cbxSurata
-            // 
-            this.cbxSurata.FormattingEnabled = true;
-            this.cbxSurata.Location = new System.Drawing.Point(136, 24);
-            this.cbxSurata.Name = "cbxSurata";
-            this.cbxSurata.Size = new System.Drawing.Size(110, 21);
-            this.cbxSurata.TabIndex = 1;
-            // 
-            // lblVersoAlcorao
-            // 
-            this.lblVersoAlcorao.AutoSize = true;
-            this.lblVersoAlcorao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblVersoAlcorao.Location = new System.Drawing.Point(6, 82);
-            this.lblVersoAlcorao.Name = "lblVersoAlcorao";
-            this.lblVersoAlcorao.Size = new System.Drawing.Size(116, 17);
-            this.lblVersoAlcorao.TabIndex = 2;
-            this.lblVersoAlcorao.Text = "Informe o Verso: ";
-            // 
-            // txbVersiculoAlcorao
-            // 
-            this.txbVersiculoAlcorao.Location = new System.Drawing.Point(146, 81);
-            this.txbVersiculoAlcorao.Name = "txbVersiculoAlcorao";
-            this.txbVersiculoAlcorao.Size = new System.Drawing.Size(100, 20);
-            this.txbVersiculoAlcorao.TabIndex = 3;
-            // 
-            // btnProcuraAlcorao
-            // 
-            this.btnProcuraAlcorao.Location = new System.Drawing.Point(85, 152);
-            this.btnProcuraAlcorao.Name = "btnProcuraAlcorao";
-            this.btnProcuraAlcorao.Size = new System.Drawing.Size(75, 23);
-            this.btnProcuraAlcorao.TabIndex = 4;
-            this.btnProcuraAlcorao.Text = "Procurar";
-            this.btnProcuraAlcorao.UseVisualStyleBackColor = true;
-            // 
-            // lblLivro
-            // 
-            this.lblLivro.AutoSize = true;
-            this.lblLivro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblLivro.Location = new System.Drawing.Point(6, 20);
-            this.lblLivro.Name = "lblLivro";
-            this.lblLivro.Size = new System.Drawing.Size(125, 17);
-            this.lblLivro.TabIndex = 0;
-            this.lblLivro.Text = "Selecione o Livro: ";
-            // 
-            // txbCapituloBiblia
-            // 
-            this.txbCapituloBiblia.Location = new System.Drawing.Point(142, 68);
-            this.txbCapituloBiblia.Name = "txbCapituloBiblia";
-            this.txbCapituloBiblia.Size = new System.Drawing.Size(104, 20);
-            this.txbCapituloBiblia.TabIndex = 3;
-            this.txbCapituloBiblia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(6, 68);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Informe o Capítulo: ";
+            this.cbxLivrosBiblia.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbxLivrosBiblia.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbxLivrosBiblia.FormattingEnabled = true;
+            this.cbxLivrosBiblia.Location = new System.Drawing.Point(131, 19);
+            this.cbxLivrosBiblia.Name = "cbxLivrosBiblia";
+            this.cbxLivrosBiblia.Size = new System.Drawing.Size(115, 21);
+            this.cbxLivrosBiblia.TabIndex = 6;
             // 
             // txbVersiculoBiblia
             // 
@@ -208,23 +120,117 @@
             this.lblVersiculo.TabIndex = 4;
             this.lblVersiculo.Text = "Informe o Versiculo: ";
             // 
-            // cbxLivrosBiblia
+            // txbCapituloBiblia
             // 
-            this.cbxLivrosBiblia.FormattingEnabled = true;
-            this.cbxLivrosBiblia.Location = new System.Drawing.Point(131, 19);
-            this.cbxLivrosBiblia.Name = "cbxLivrosBiblia";
-            this.cbxLivrosBiblia.Size = new System.Drawing.Size(115, 21);
-            this.cbxLivrosBiblia.TabIndex = 6;
+            this.txbCapituloBiblia.Location = new System.Drawing.Point(142, 68);
+            this.txbCapituloBiblia.Name = "txbCapituloBiblia";
+            this.txbCapituloBiblia.Size = new System.Drawing.Size(104, 20);
+            this.txbCapituloBiblia.TabIndex = 3;
+            this.txbCapituloBiblia.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // btnProcuraBiblia
+            // label2
             // 
-            this.btnProcuraBiblia.Location = new System.Drawing.Point(90, 165);
-            this.btnProcuraBiblia.Name = "btnProcuraBiblia";
-            this.btnProcuraBiblia.Size = new System.Drawing.Size(75, 23);
-            this.btnProcuraBiblia.TabIndex = 7;
-            this.btnProcuraBiblia.Text = "Procurar";
-            this.btnProcuraBiblia.UseVisualStyleBackColor = true;
-            this.btnProcuraBiblia.Click += new System.EventHandler(this.btnProcuraBiblia_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(6, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Informe o Capítulo: ";
+            // 
+            // lblLivro
+            // 
+            this.lblLivro.AutoSize = true;
+            this.lblLivro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblLivro.Location = new System.Drawing.Point(6, 20);
+            this.lblLivro.Name = "lblLivro";
+            this.lblLivro.Size = new System.Drawing.Size(125, 17);
+            this.lblLivro.TabIndex = 0;
+            this.lblLivro.Text = "Selecione o Livro: ";
+            // 
+            // tbpAlcorao
+            // 
+            this.tbpAlcorao.Controls.Add(this.btnProcuraAlcorao);
+            this.tbpAlcorao.Controls.Add(this.txbVersiculoAlcorao);
+            this.tbpAlcorao.Controls.Add(this.lblVersoAlcorao);
+            this.tbpAlcorao.Controls.Add(this.cbxSurata);
+            this.tbpAlcorao.Controls.Add(this.lblSurata);
+            this.tbpAlcorao.Location = new System.Drawing.Point(4, 22);
+            this.tbpAlcorao.Name = "tbpAlcorao";
+            this.tbpAlcorao.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpAlcorao.Size = new System.Drawing.Size(252, 211);
+            this.tbpAlcorao.TabIndex = 1;
+            this.tbpAlcorao.Text = "Alcorão";
+            this.tbpAlcorao.UseVisualStyleBackColor = true;
+            // 
+            // btnProcuraAlcorao
+            // 
+            this.btnProcuraAlcorao.Location = new System.Drawing.Point(85, 152);
+            this.btnProcuraAlcorao.Name = "btnProcuraAlcorao";
+            this.btnProcuraAlcorao.Size = new System.Drawing.Size(75, 23);
+            this.btnProcuraAlcorao.TabIndex = 4;
+            this.btnProcuraAlcorao.Text = "Procurar";
+            this.btnProcuraAlcorao.UseVisualStyleBackColor = true;
+            // 
+            // txbVersiculoAlcorao
+            // 
+            this.txbVersiculoAlcorao.Location = new System.Drawing.Point(146, 81);
+            this.txbVersiculoAlcorao.Name = "txbVersiculoAlcorao";
+            this.txbVersiculoAlcorao.Size = new System.Drawing.Size(100, 20);
+            this.txbVersiculoAlcorao.TabIndex = 3;
+            // 
+            // lblVersoAlcorao
+            // 
+            this.lblVersoAlcorao.AutoSize = true;
+            this.lblVersoAlcorao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblVersoAlcorao.Location = new System.Drawing.Point(6, 82);
+            this.lblVersoAlcorao.Name = "lblVersoAlcorao";
+            this.lblVersoAlcorao.Size = new System.Drawing.Size(116, 17);
+            this.lblVersoAlcorao.TabIndex = 2;
+            this.lblVersoAlcorao.Text = "Informe o Verso: ";
+            // 
+            // cbxSurata
+            // 
+            this.cbxSurata.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbxSurata.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbxSurata.FormattingEnabled = true;
+            this.cbxSurata.Location = new System.Drawing.Point(136, 24);
+            this.cbxSurata.Name = "cbxSurata";
+            this.cbxSurata.Size = new System.Drawing.Size(110, 21);
+            this.cbxSurata.TabIndex = 1;
+            // 
+            // lblSurata
+            // 
+            this.lblSurata.AutoSize = true;
+            this.lblSurata.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblSurata.Location = new System.Drawing.Point(6, 25);
+            this.lblSurata.Name = "lblSurata";
+            this.lblSurata.Size = new System.Drawing.Size(136, 17);
+            this.lblSurata.TabIndex = 0;
+            this.lblSurata.Text = "Selecione a Surata: ";
+            // 
+            // tbpNotas
+            // 
+            this.tbpNotas.Controls.Add(this.btnEditarNota);
+            this.tbpNotas.Controls.Add(this.btnExcluirNota);
+            this.tbpNotas.Controls.Add(this.dgvNotas);
+            this.tbpNotas.Location = new System.Drawing.Point(4, 22);
+            this.tbpNotas.Name = "tbpNotas";
+            this.tbpNotas.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpNotas.Size = new System.Drawing.Size(252, 211);
+            this.tbpNotas.TabIndex = 2;
+            this.tbpNotas.Text = "Notas";
+            this.tbpNotas.UseVisualStyleBackColor = true;
+            // 
+            // btnEditarNota
+            // 
+            this.btnEditarNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnEditarNota.Location = new System.Drawing.Point(139, 182);
+            this.btnEditarNota.Name = "btnEditarNota";
+            this.btnEditarNota.Size = new System.Drawing.Size(96, 23);
+            this.btnEditarNota.TabIndex = 2;
+            this.btnEditarNota.Text = "Editar Nota";
+            this.btnEditarNota.UseVisualStyleBackColor = true;
             // 
             // btnExcluirNota
             // 
@@ -236,15 +242,13 @@
             this.btnExcluirNota.Text = "Excluir Nota";
             this.btnExcluirNota.UseVisualStyleBackColor = true;
             // 
-            // btnEditarNota
+            // dgvNotas
             // 
-            this.btnEditarNota.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnEditarNota.Location = new System.Drawing.Point(139, 182);
-            this.btnEditarNota.Name = "btnEditarNota";
-            this.btnEditarNota.Size = new System.Drawing.Size(96, 23);
-            this.btnEditarNota.TabIndex = 2;
-            this.btnEditarNota.Text = "Editar Nota";
-            this.btnEditarNota.UseVisualStyleBackColor = true;
+            this.dgvNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNotas.Location = new System.Drawing.Point(6, 6);
+            this.dgvNotas.Name = "dgvNotas";
+            this.dgvNotas.Size = new System.Drawing.Size(240, 169);
+            this.dgvNotas.TabIndex = 0;
             // 
             // frmPrincipal
             // 
